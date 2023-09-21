ebe77582a7ad2a39e3e1adaa8adafb230e95c8fe
Nishant Banjade <nishantbanjade07@gmail.com>
2023-09-21 16:57:11 +0530

    test: Add unit tests for UserProfileNavMobileView and UserProfileNav components
    Description:
    - Created unit tests to ensure the correct rendering and behavior of UserProfileNavMobileView and UserProfileNav components.
    - Verified that icons are displayed as expected.
    - Tested the toggle functionality to confirm that the menu opens and closes correctly.
    - Checked the responsiveness of both components on various screen sizes.
    
    This commit adds comprehensive unit tests for UserProfileNavMobileView and UserProfileNav components, ensuring their functionality, icon rendering, toggle behavior, and responsiveness.

--- src/__test__/UserProfileNav/UserProfileNavMobileView.test.tsx
+++ src/__test__/UserProfileNav/UserProfileNavMobileView.test.tsx
@@ -1,28 +1,41 @@
-import { render , screen} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import UserProfileNavMobileView from "../../component/UserProfileNav/UserProfileNavMobileView";
 import UserInformationDataForNavBar from "../../assets/config/developer_data/customizedev";
 
 describe("UserProfileNavMobileView Component", () => {
-    test("developer_username should have the correct value", () => {
-      const { queryByText } = render(<UserProfileNavMobileView />);
-      const { developer_username } = UserInformationDataForNavBar;
-      const developerUsernameElement = queryByText(developer_username);
-      expect(developerUsernameElement).not.toBeNull();
-    });
+  test("developer_username should have the correct value", () => {
+    const { queryByText } = render(<UserProfileNavMobileView />);
+    const { developer_username } = UserInformationDataForNavBar;
+    const developerUsernameElement = queryByText(developer_username);
+    expect(developerUsernameElement).not.toBeNull();
+  });
 
-    test("Should correctly render the Menu Icon in the Mobile View", () => {
+  test("Should correctly render the Menu Icon in the Mobile View", () => {
+    render(<UserProfileNavMobileView />);
+    const menuIcon = screen.getByLabelText("menu-icon");
+    expect(menuIcon).not.toBeNull();
+  });
 
-        render(<UserProfileNavMobileView/>);
-        const menuIcon = screen.getByLabelText("menu-icon");
-        expect(menuIcon).not.toBeNull();
-        
-    });
+  test("Should correctly render the Terminal Icon in the Mobile View", () => {
+    render(<UserProfileNavMobileView />);
+    const terminalIcon = screen.getByLabelText("terminal-icon");
+    expect(terminalIcon).not.toBeNull();
+  });
 
-    test("Should correctly render the Terminal Icon in the Mobile View", () => {
+  test("Toggle menu should open and close the menu", () => {
+    const { getByLabelText, queryByLabelText } = render(
+      <UserProfileNavMobileView />
+    );
 
-        render(<UserProfileNavMobileView/>);
-        const terminalIcon = screen.getByLabelText("terminal-icon");
-        expect(terminalIcon).not.toBeNull();
-        
-    })
-})
+    // check if the menu is initially closed
+    expect(queryByLabelText("close-menu-icon")).toBeNull();
+    // click on the menu icon to open the menu
+    fireEvent.click(getByLabelText("menu-icon"));
+    // close menu is visible
+    expect(queryByLabelText("close-menu-icon")).not.toBeNull();
+    // click close icon
+    fireEvent.click(getByLabelText("close-menu-icon"));
+    // menu icon is visible
+    expect(queryByLabelText("menu-icon")).not.toBeNull();
+  });
+});
