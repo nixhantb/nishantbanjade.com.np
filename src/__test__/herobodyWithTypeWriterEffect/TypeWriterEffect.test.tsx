f993149593bef97aab77bb18a21137a061e70c8b
Nishant Banjade <nishantbanjade07@gmail.com>
2023-09-24 15:43:54 +0530

    chore: updating the test by utilizing the typeWriter_speed props

--- src/__test__/herobodyWithTypeWriterEffect/TypeWriterEffect.test.tsx
+++ src/__test__/herobodyWithTypeWriterEffect/TypeWriterEffect.test.tsx
@@ -1,11 +1,17 @@
 import TypeWriterEffect from "../../component/herobodyWithTypeWriterEffect/typewriter/TypeWriterEffect";
 import { render, screen, waitFor } from "@testing-library/react";
 
+const typeWriter_speed = 22;
 describe("TypeWriterEffect component", () => {
   it("should type out the full text", async () => {
     const fullText = "Hello world";
 
-    render(<TypeWriterEffect fullText={fullText} />);
+    render(
+      <TypeWriterEffect
+        fullText={fullText}
+        typeWriterSpeed={typeWriter_speed}
+      />
+    );
 
     // Wait for the typing effect to finish.
     await waitFor(() => {
@@ -15,19 +21,32 @@ describe("TypeWriterEffect component", () => {
 
   it("should display null for null text", async () => {
     const NullText = "";
-    render(<TypeWriterEffect fullText={NullText} />);
+    render(
+      <TypeWriterEffect
+        fullText={NullText}
+        typeWriterSpeed={typeWriter_speed}
+      />
+    );
     await waitFor(() => {
       expect(screen.queryByText(NullText.length)).toBeNull();
     });
   });
-  
+
   it("should type out a long text", async () => {
     const longText = "This is a very long text that should be typed out.";
 
-    render(<TypeWriterEffect fullText={longText} />);
+    render(
+      <TypeWriterEffect
+        fullText={longText}
+        typeWriterSpeed={typeWriter_speed}
+      />
+    );
 
-    await waitFor(() => {
-      expect(screen.getByText(longText)).toBeTruthy();
-    }, { timeout: 10000 }); // Adjust the timeout as needed for very long text.
+    await waitFor(
+      () => {
+        expect(screen.getByText(longText)).toBeTruthy();
+      },
+      { timeout: 10000 }
+    ); // Adjust the timeout as needed for very long text.
   });
 });
