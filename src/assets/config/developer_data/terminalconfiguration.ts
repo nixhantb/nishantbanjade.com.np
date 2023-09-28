86c03f5b6276ed287df609a8bb8b7e25e9de9b10
Nishant Banjade <nishantbanjade07@gmail.com>
2023-09-28 16:37:22 +0530

    feat: Update TerminalConfiguration
    - Add welcome_message and terminal_username
    - Provide instructions for command formatting
    - Modify 'whoami' and 'education' fields for consistency
    
    This commit updates the TerminalConfiguration file with the following changes:
    1. Added 'welcome_message' and 'terminal_username' fields for terminal initialization.
    2. Included a helpful comment suggesting the use of '<br/>' at the end of commands for a response in the next line.
    3. Modified the 'whoami' and 'education' fields for consistency by removing '<br/>' from the 'company' field.
    
    These changes improve the clarity and usability of the TerminalConfiguration.

---
+++ src/assets/config/developer_data/terminalconfiguration.ts
@@ -0,0 +1,17 @@
+const TerminalConfiguration = {
+  welcome_message: "Type 'help' to get started",
+  terminal_username: "[Nix-term~ ]",
+  // Please add <br/> at the end of your command to get response in the next line
+  whoami: {
+    name: "Nishant Banjade <br/>",
+    Profession: "Software Engineer I <br/>",
+    company: "Ellucian | Bangalore",
+  },
+  education: {
+    title: "CSE(hons.) Data Science <br/>",
+    desc: "Jain (Deemed-to-be) University | 2019 - 2023 <br/>",
+    CGPA: "8.4 / 10",
+  },
+};
+
+export default TerminalConfiguration;
