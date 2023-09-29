54e8ef338056e2c99418e95db64faa02d88e0545
Nishant Banjade <nishantbanjade07@gmail.com>
2023-09-29 18:55:23 +0530

    remove Terminal.test.tsx for figuring the issue in vercel failed deployment issue

--- src/__test__/terminal/terminal.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import Terminal from "../../component/terminal/Terminal";
-
-test("Renders the terminal page", () => {
-  render(<Terminal />);
-  expect(true).toBeTruthy();
-});
