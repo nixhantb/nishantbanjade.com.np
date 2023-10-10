5baf8df3e2c1f31c17e03e80b17d6441aa0bec65
Nishant Banjade <nishantbanjade07@gmail.com>
2023-10-10 13:25:10 +0530

    delete: Project Stories for navbar and data folder for config which was no longer in use

--- src/assets/config/data/customizeData.ts
+++
@@ -1,8 +0,0 @@
-const customizedDeveloperData = {
-  developer_username: "your_user_name",
-  navbar_home: "home",
-  navbar_about: "about",
-  navbar_contact: "contact",
-  navbar_terminal: "terminal",
-};
-export default customizedDeveloperData;
